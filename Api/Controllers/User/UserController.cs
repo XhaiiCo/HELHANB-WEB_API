9b2898afefffc157d17d3b623317fe16f1774b6c
Leclercq David <davidleclercq2002@gmail.com>
2022-11-03 13:30:03 +0100

    Create the controller for users

---
+++ Api/Controllers/User/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.User;
+
+[ApiController]
+[Route("api/v1/users")]
+public class UserController: ControllerBase
+{
+    [HttpGet]
+    public ActionResult FetchAll()
+    {
+        return Ok();
+    }
+
+}
