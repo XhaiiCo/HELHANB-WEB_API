f2321fef9e53a19a1f2a7eadb13195578111d88f
Leclercq David <davidleclercq2002@gmail.com>
2022-11-03 13:35:24 +0100

    Create UserRepository.cs

---
+++ Infrastructure/Ef/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Utils;
+using Microsoft.Identity.Client;
+
+namespace Infrastructure.Ef.DbEntities;
+
+public class UserRepository: IUserRepository
+{
+    private readonly HelhanbContextProvider _contextProvider;
+
+    public UserRepository(HelhanbContextProvider contextProvider)
+    {
+        _contextProvider = contextProvider;
+    }
+
+    public IEnumerable<DbUser> FetchAll()
+    {
+        using var context = _contextProvider.NewContext() ;
+        return context.Users.ToList() ;
+    }
+}
