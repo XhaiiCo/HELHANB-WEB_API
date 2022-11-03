0c52fa46270dfec227101ce1f407524b3baa1792
Leclercq David <davidleclercq2002@gmail.com>
2022-11-03 13:29:46 +0100

    Create the domain for user (not finish)

---
+++ Domain/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+//    public DateTime AccountCreation { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public int RoleId { get; set; }
+
+}
