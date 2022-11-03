7b4f70ff4835c39340daee63456fa4fb4b6fea10
Leclercq David <davidleclercq2002@gmail.com>
2022-11-03 13:40:16 +0100

    Add services

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,4 +1,6 @@
 using API;
+using Infrastructure.Ef;
+using Infrastructure.Ef.DbEntities;
 using Infrastructure.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +14,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IConnectionStringProvider, ConnectionStringProvider>();
 builder.Services.AddScoped<HelhanbContextProvider>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
