e3aa921797e32dffc8d5110545ebce305d02d375
Leclercq David <davidleclercq2002@gmail.com>
2022-11-03 21:27:44 +0100

    Hash the user password at created

--- Application/UseCases/Users/UseCaseCreateUser.cs
+++ Application/UseCases/Users/UseCaseCreateUser.cs
@@ -1,4 +1,5 @@
-﻿using Application.Services.User;
+﻿using Application.Services.Auth;
+using Application.Services.User;
 using Application.UseCases.Users.Dtos;
 using Application.UseCases.Utils;
 using Infrastructure.Ef;
@@ -10,11 +11,13 @@ public class UseCaseCreateUser: IUseCaseWriter<DtoOutputUser?, DtoInputCreateUse
 {
     private readonly IUserRepository _userRepository;
     private readonly IUserService _userService;
+    private readonly IAuthService _authService;
 
-    public UseCaseCreateUser(IUserRepository userRepository, IUserService userService)
+    public UseCaseCreateUser(IUserRepository userRepository, IUserService userService, IAuthService authService)
     {
         _userRepository = userRepository;
         _userService = userService;
+        _authService = authService;
     }
 
     public DtoOutputUser? Execute(DtoInputCreateUser input)
@@ -30,6 +33,8 @@ public class UseCaseCreateUser: IUseCaseWriter<DtoOutputUser?, DtoInputCreateUse
             DbUser user = Mapper.GetInstance().Map<DbUser>(input);
             user.AccountCreation = DateTime.Now;
             user.RoleId = 1 ;
+            //hash password
+            user.Password = _authService.HashPassword(user.Password);
             var userInDb = _userRepository.Create(user);
             return Mapper.GetInstance().Map<DtoOutputUser>(userInDb);
         }
