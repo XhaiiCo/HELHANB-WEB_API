107870c784a6c3aea98add57ad7fcc2f056151ae
Strafs2 <guillaumedelrot@gmail.com>
2022-11-04 13:18:11 +0100

    Create the classes of base

---
+++ Domain/Reservation.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+
+public class Reservation
+{
+    public int Id { get; set; }
+ //   public DateTime Creation { get; set; }
+ //  public DateOnly ArrivalDate { get; set; }
+ // public DateOnly LeaveDate { get; set; }
+    
+}
