06aff547049ecc1432b0ba349094a50ef8b9741e
Leclercq David <davidleclercq2002@gmail.com>
2022-11-12 10:57:02 +0100

    Create roleRepository

---
+++ Infrastructure/Ef/Repository/Role/IRoleRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Infrastructure.Ef.Repository;
+
+public interface IRoleRepository
+{
+    
+}
---
+++ Infrastructure/Ef/Repository/Role/RoleRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Infrastructure.Ef.Repository;
+
+public class RoleRepository
+{
+    
+}
