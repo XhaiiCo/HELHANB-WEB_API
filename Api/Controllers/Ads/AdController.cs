b9389f377381d1b9dbb171a0cdf7bd1ae047ac73
Strafs2 <guillaumedelrot@gmail.com>
2022-11-14 14:22:06 +0100

    Create AdController

---
+++ Api/Controllers/Ads/AdController.cs
@@ -0,0 +1,27 @@
+﻿using Application.UseCases.Ads;
+using Application.UseCases.Ads.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.Ads;
+
+[ApiController]
+[Route("api/v1/ad")]
+public class AdController
+{
+    private readonly UseCaseCreateAd _useCaseCreateAd;
+
+
+    public AdController(UseCaseCreateAd useCaseCreateAd)
+    {
+        _useCaseCreateAd = useCaseCreateAd;
+    }
+
+    
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public ActionResult<DtoOutputAd> Create(DtoInputCreateAd dto)
+    {
+        return (_useCaseCreateAd.Execute(dto));
+    }
+
+}
