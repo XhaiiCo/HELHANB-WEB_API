5e3743b4810283da5101fe831cd3e6164b61d575
JeremSSJ <jeremssj7@gmail.com>
2022-11-16 17:49:50 +0100

    reservation test

--- Tests/ReservationTests.cs
+++ Tests/ReservationTests.cs
@@ -5,21 +5,24 @@ namespace Tests;
 public class ReservationTests
 {
     [Test]
-    [TestCase("01/01/2022", "13/01/2022", false)]
-    [TestCase("14/01/2022", "17/01/2022", true)]
+    [TestCase("01/01/2022 07:00", "01/01/2022 11:00", false)]
+    [TestCase("13/01/2022 21:00", "17/01/2022 22:00", false)]
+    [TestCase("13/01/2022 23:00", "17/01/2022 22:00", true)]
     public void IsDateAvailableTest(string dateArrival, string dateLeave, bool expected)
     {
         List<Reservation> reservations = new List<Reservation>();
         
         reservations.Add(new Reservation
         {
-            ArrivalDate = new DateOnly(2022, 01, 01), 
-            LeaveDate = new DateOnly(2022, 01, 13)
+            ArrivalDate = new DateTime(2022, 01, 01, 10, 0, 0), 
+            LeaveDate = new DateTime(2022, 01, 13, 22, 0, 0)
         });
         
         Assert.That(Reservation.IsDateAvailable(reservations, new Reservation
         {
-            ArrivalDate = DateOnly.Parse(dateArrival), LeaveDate = DateOnly.Parse(dateLeave)
+            ArrivalDate = DateTime.Parse(dateArrival), LeaveDate = DateTime.Parse(dateLeave)
         }), Is.EqualTo(expected));
+        
+        
     }
 }
