a1b154d94e1b2ffca08eb3c326369001f5d27724
Leclercq David <davidleclercq2002@gmail.com>
2022-11-30 16:08:44 +0100

    Chat works pog

--- Api/ChatController/ChatHub.cs
+++ Api/ChatController/ChatHub.cs
@@ -3,17 +3,24 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace API.ChatController;
 
-[Authorize]
 public class ChatHub : Hub // inherit this
 {
+    [Authorize]
     public override Task OnConnectedAsync()
-    { 
+    {
         Groups.AddToGroupAsync(Context.ConnectionId, Context.User.Identity.Name);
         return base.OnConnectedAsync();
     }
-    
+
+    [Authorize]
     public Task SendMessage1(string user, string message, string group) // Two parameters accepted
     {
-        return Clients.Groups(Context.User.Identity.Name).SendAsync("ReceiveOne", user, message); // Note this 'ReceiveOne' 
+        return Clients.Group(group).SendAsync("ReceiveOne", user, message); // Note this 'ReceiveOne' 
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        Groups.RemoveFromGroupAsync(Context.ConnectionId, Context.User.Identity.Name);
+        return base.OnDisconnectedAsync(exception);
     }
 }
