12a56a0629f0dac3192d43e3f5c4a00d2c4d2fc0
JeremSSJ <jeremssj7@gmail.com>
2022-12-08 13:16:08 +0100

    delete pictures when deleting ads

--- Application/UseCases/Ads/UseCaseDeleteAd.cs
+++ Application/UseCases/Ads/UseCaseDeleteAd.cs
@@ -1,18 +1,23 @@
-﻿using Application.UseCases.Ads.Dtos;
+﻿using API.Utils.Picture;
+using Application.UseCases.Ads.Dtos;
 using Application.UseCases.Utils;
 using Infrastructure.Ef.DbEntities;
 using Infrastructure.Ef.Repository.Ad;
+using Infrastructure.Ef.Repository.AdPicture;
 
 namespace Application.UseCases.Ads;
 
 public class UseCaseDeleteAd : IUseCaseParameterizedQuery<DtoOutputAd, string>
 {
     private readonly IAdRepository _adRepository;
+    private readonly IAdPictureRepository _adPictureRepository;
+    private readonly IPictureService _pictureService;
 
-
-    public UseCaseDeleteAd(IAdRepository adRepository)
+    public UseCaseDeleteAd(IAdRepository adRepository, IAdPictureRepository adPictureRepository, IPictureService pictureService)
     {
         _adRepository = adRepository;
+        _adPictureRepository = adPictureRepository;
+        _pictureService = pictureService;
     }
 
     public DtoOutputAd Execute(string slug)
@@ -21,8 +26,17 @@ public class UseCaseDeleteAd : IUseCaseParameterizedQuery<DtoOutputAd, string>
 
         var dbAd = Mapper.GetInstance().Map<DbAd>(ad);
 
+        var dbAdPictures = _adPictureRepository.FetchByAdId(dbAd.Id);
+        
+        foreach (var dbAdPicture in dbAdPictures)
+        {
+            _adPictureRepository.Delete(dbAdPicture);
+                
+            _pictureService.RemoveFile(dbAdPicture.Path);
+        }
+        
         _adRepository.Delete(dbAd);
-
+        
         return Mapper.GetInstance().Map<DtoOutputAd>(ad);
     }
 }
