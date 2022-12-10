b39d439c40328185a6440e6193d27355c2affa3f
JeremSSJ <jeremssj7@gmail.com>
2022-12-10 15:44:25 +0100

    refactor a bit ad filtration

--- Application/Services/Ad/IAdService.cs
+++ Application/Services/Ad/IAdService.cs
@@ -10,7 +10,7 @@ public interface IAdService
     IEnumerable<Domain.Ad> FetchByUserId(int id);
     
     IEnumerable<Domain.Ad> FetchAll(DtoInputFilteringAds dto);
-    IEnumerable<Domain.Ad> FetchRange(int offset, int limit, DtoInputFilteringAds filter);
+    IEnumerable<Domain.Ad> FetchRange(DtoInputFilteringAds filter);
 
     Domain.Ad MapToAd(DbAd dbAd);
 }
--- Infrastructure/Ef/Repository/Ad/FilteringAd.cs
+++ Infrastructure/Ef/Repository/Ad/FilteringAd.cs
@@ -2,6 +2,8 @@
 
 public class FilteringAd
 {
+    public int Limit { get; set; }
+    public int Offset { get; set; }
     public int? StatusId { get; set; }
     public string? Country { get; set; }
 }
