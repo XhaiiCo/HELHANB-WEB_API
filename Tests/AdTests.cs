fb3a00b37f6a4f0a07dd9a4cbd199a382a025543
JeremSSJ <jeremssj7@gmail.com>
2022-12-18 14:03:36 +0100

    correct reservation test

--- Tests/AdTests.cs
+++ Tests/AdTests.cs
@@ -30,38 +30,4 @@ public class AdTests
     {
         Assert.That(Ad.ValidHours(TimeSpan.Parse(arrivalStart), TimeSpan.Parse(arrivalEnd), TimeSpan.Parse(leave)), Is.EqualTo(expected));
     }
-
-    /*
-    [Test]
-    [TestCase("01/01/2022 07:00", "01/01/2022 11:00", false, 1)]
-    [TestCase("25/12/2021 07:00", "01/01/2022 07:00", true, 2)]
-    [TestCase("13/01/2022 21:00", "17/01/2022 22:00", false, 1)]
-    [TestCase("13/01/2022 23:00", "17/01/2022 22:00", true, 2)]
-    public void AddReservation(string dateArrival, string dateLeave, bool expected, int size)
-    {
-        Ad ad = new Ad();
-        ReservationBook reservationBook = new ReservationBook();
-
-        reservationBook.Add(new Reservation
-        {
-            DateTimeRange = new DateTimeRange
-            (
-                new DateTime(2022, 01, 01, 10, 0, 0),
-                new DateTime(2022, 01, 13, 22, 0, 0)
-            )
-        });
-
-        bool testAddReservation = reservationBook.Add(new Reservation
-        {
-            DateTimeRange = new DateTimeRange
-            (
-                DateTime.Parse(dateArrival),
-                DateTime.Parse(dateLeave)
-            )
-        });
-        
-        Assert.That(testAddReservation, Is.EqualTo(expected));
-
-        Assert.That(reservationBook.Entries(), Has.Exactly(size).Items);
-    }*/
 }
--- Tests/ReservationTests.cs
+++ Tests/ReservationTests.cs
@@ -46,30 +46,35 @@ public class ReservationTests
     }
     
     [Test]
-    [TestCase("21/11/2022 00:00", false)]
-    [TestCase("23/11/2022 00:00", true)]//Always replace with today day
-    public void IsForTheSameDayTest(string arrivalDate, bool expected)
+    [TestCase(-1,false)]
+    [TestCase(0,true)]
+    [TestCase(1,false)]
+    public void IsForTheSameDayTest(int dayDiff, bool expected)
     {
+        var arrivalDate = DateTime.Now.AddDays(dayDiff);
+        
         Assert.That(Reservation.IsForTheSameDay(new Reservation
         {
             DateTimeRange = new DateTimeRange
             {
-                ArrivalDate = DateTime.Parse(arrivalDate)
+                ArrivalDate = arrivalDate
             }
         }), Is.EqualTo(expected));
     }
 
     [Test]
-    [TestCase("21/11/2022 00:00", true)]
-    [TestCase("23/11/2022 19:00", false)]//Always replace with today day and highter hour
-    [TestCase("22/11/2100 00:00", false)]
-    public void IsInThePastTest(string arrivalDate, bool expected)
+    [TestCase(-1, true)]
+    [TestCase(0, true)]
+    [TestCase(1, false)]
+    public void IsInThePastTest(int secondDiff, bool expected)
     {
+        var arrivalDate = DateTime.Now.AddSeconds(secondDiff);
+        
         Assert.That(Reservation.IsInThePast(new Reservation
         {
             DateTimeRange = new DateTimeRange
             {
-                ArrivalDate = DateTime.Parse(arrivalDate)
+                ArrivalDate = arrivalDate
             }
         }), Is.EqualTo(expected));
     }
@@ -95,32 +100,38 @@ public class ReservationTests
     }
 
     [Test]
-    [TestCase("23/11/2021 19:00", "24/11/2021 23:59:59", "La date d'arrivée ne peut pas être passée")]
-    [TestCase("23/11/2022 19:00", "24/11/2100 23:59:59","Une réservation doit être au minimum pour le lendemain")] //date of today
-    [TestCase("24/11/2100 00:00", "24/11/2100 23:59:59", "La réservation doit au moins faire une nuit")]//a date above the actual date
-    public void ValidNewReservationThrowsExceptionTest(string arrivalDate, string leaveDate, string expectedErrorMessage)
+    [TestCase(0,-1, 1,0, "La date d'arrivée ne peut pas être passée")]
+    [TestCase(0,10, 1,10,"Une réservation doit être au minimum pour le lendemain")]
+    [TestCase(1,0, 1,1, "La réservation doit au moins faire une nuit")]
+    public void ValidNewReservationThrowsExceptionTest(int dayDiff1, int secondDiff1, int dayDiff2, int secondDiff2, string expectedErrorMessage)
     {
+        var arrivalDate = DateTime.Now.AddDays(dayDiff1).AddSeconds(secondDiff1);
+        var leaveDate = DateTime.Now.AddDays(dayDiff2).AddSeconds(secondDiff2);
+        
         var exception = Assert.Throws<Exception>(() => Reservation.ValidNewReservation(new Reservation
         {
             DateTimeRange = new DateTimeRange
             {
-                ArrivalDate = DateTime.Parse(arrivalDate),
-                LeaveDate = DateTime.Parse(leaveDate)
+                ArrivalDate = arrivalDate,
+                LeaveDate = leaveDate
             }
         }));
         Assert.That(exception.Message, Is.EqualTo(expectedErrorMessage));
     }
 
     [Test]
-    [TestCase("24/11/2022 00:00", "23/11/2100 00:00", true)]
-    public void ValidNewReservationTest(string arrivalDate, string leaveDate, bool expected)
+    [TestCase(1," 00:00",2, " 00:00", true)]
+    public void ValidNewReservationTest(int dayDiff1, string arrivalHour, int dayDiff2, string leaveHour, bool expected)
     {
+        var arrivalDate = DateTime.Now.AddDays(dayDiff1).ToString("dd/MM/yyyy");;
+        var leaveDate = DateTime.Now.AddDays(dayDiff2).ToString("dd/MM/yyyy");;
+        
         Assert.That(Reservation.ValidNewReservation(new Reservation
         {
             DateTimeRange = new DateTimeRange
             {
-                ArrivalDate = DateTime.Parse(arrivalDate),
-                LeaveDate = DateTime.Parse(leaveDate)
+                ArrivalDate = DateTime.Parse(arrivalDate + arrivalHour),
+                LeaveDate = DateTime.Parse(leaveDate + leaveHour)
             }
         }), Is.EqualTo(expected));
     }
